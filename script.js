0746f085b9dac98cc9cde7bb2c31cab6ee8d796d
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-10 08:00:24 +0100

    Add UI and extra functions
    Make the game playable with buttons and a function for reseting the game
    when finished.

--- script.js
+++ script.js
@@ -1,60 +1,87 @@
 var playerScore = 0;
 var computerScore = 0;
+var inputButtons = document.querySelectorAll(".player-button");
+var playerScoreDiv = document.querySelector(".player-score span");
+var computerScoreDiv = document.querySelector(".computer-score span");
+var winnerPresentation = document.querySelector(".winner-presentation");
+var scoreBoard = document.querySelector(".score-board");
+inputButtons.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        winnerPresentation.innerText = playSingleRound(e.target.value, getComputerChoice());
+        if (playerScore === 5 || computerScore === 5) {
+            endGame(playerScore - computerScore);
+        }
+    });
+});
 // Randomly generate rock, paper og scissor
 function getComputerChoice() {
     var randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 1) {
-        return "Rock";
+        return "rock";
     }
     else if (randomNum === 2) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissor";
+        return "scissor";
     }
 }
-// Play a single round and declare winner
-function playSingleGame(playerSelection, computerSelection) {
-    var playerChoice = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    var computerChoice = computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1).toLowerCase();
+// Play a single round and declare winner of round
+function playSingleRound(playerSelection, computerSelection) {
+    var playerChoice = playerSelection;
+    var computerChoice = computerSelection;
     var messageDraw = "Its a draw! ".concat(playerChoice, " vs ").concat(computerChoice);
     var messagePlayerWin = "The player wins! ".concat(playerChoice, " vs ").concat(computerChoice);
     var messageComputerWin = "The computer wins! ".concat(computerChoice, " vs ").concat(playerChoice);
     if (playerChoice == computerChoice) {
         return messageDraw;
     }
-    else if (playerChoice == "Rock" && computerChoice != "Paper") {
+    else if (playerChoice == "rock" && computerChoice != "paper") {
         playerScore++;
+        playerScoreDiv.innerText = playerScore.toString();
         return messagePlayerWin;
     }
-    else if (playerChoice == "Scissor" && computerChoice != "Rock") {
+    else if (playerChoice == "scissor" && computerChoice != "rock") {
         playerScore++;
+        playerScoreDiv.innerText = playerScore.toString();
         return messagePlayerWin;
     }
-    else if (playerChoice == "Paper" && computerChoice != "Scissor") {
+    else if (playerChoice == "paper" && computerChoice != "scissor") {
         playerScore++;
+        playerScoreDiv.innerText = playerScore.toString();
         return messagePlayerWin;
     }
     else {
         computerScore++;
+        computerScoreDiv.innerText = computerScore.toString();
         return messageComputerWin;
     }
 }
-// Play a game
-function playGame() {
-    console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-    console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-    console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-    console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-    console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-    if (playerScore > computerScore) {
-        console.log("Congratulations player! You won with ".concat(playerScore, " points vs the computers ").concat(computerScore, " points"));
-    }
-    else if (playerScore < computerScore) {
-        console.log("I'm sorry player! You lost with ".concat(playerScore, " points vs the computers ").concat(computerScore, " points"));
+function endGame(result) {
+    inputButtons.forEach(function (btn) {
+        btn.disabled = true;
+    });
+    if (result > 0) {
+        winnerPresentation.innerText = "Game over! Congratulations player! You won with ".concat(playerScore, " points vs the computers ").concat(computerScore, " points");
     }
     else {
-        console.log("It's a tie! You both got ".concat(playerScore, " points! "));
+        winnerPresentation.innerText = "Game over! I'm sorry player! You lost with ".concat(playerScore, " points vs the computers ").concat(computerScore, " points");
     }
+    var resetBtn = document.createElement("button");
+    resetBtn.value = "play-again";
+    resetBtn.textContent = "Play again";
+    resetBtn.classList.add("reset-button");
+    resetBtn.addEventListener("click", function () {
+        resetGame(resetBtn);
+    });
+    scoreBoard.appendChild(resetBtn);
 }
-playGame();
+function resetGame(element) {
+    playerScore = 0;
+    computerScore = 0;
+    computerScoreDiv.innerText = computerScore.toString();
+    playerScoreDiv.innerText = playerScore.toString();
+    inputButtons.forEach(function (e) { return e.disabled = false; });
+    scoreBoard.removeChild(element);
+}
+;
--- script.ts
+++ script.ts
@@ -1,55 +1,87 @@
 let playerScore: number = 0;
 let computerScore: number = 0;
+const inputButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll(".player-button");
+const playerScoreDiv: HTMLElement = document.querySelector(".player-score span");
+const computerScoreDiv: HTMLElement = document.querySelector(".computer-score span");
+const winnerPresentation: HTMLElement = document.querySelector(".winner-presentation");
+const scoreBoard = document.querySelector(".score-board");	
+
+inputButtons.forEach((btn) => {
+	btn.addEventListener("click", function(e) {
+		winnerPresentation.innerText = playSingleRound((e.target as HTMLButtonElement).value, getComputerChoice());
+		if(playerScore === 5 || computerScore === 5){
+			endGame(playerScore - computerScore);
+		}
+	})
+})
 
 // Randomly generate rock, paper og scissor
 function getComputerChoice():string {
 	const randomNum: number = Math.floor(Math.random() * 3);
 	if (randomNum === 1){
-		return "Rock"
+		return "rock";
 	} else if (randomNum === 2) {
-		return "Paper";
+		return "paper";
 	} else {
-		return "Scissor"
+		return "scissor";
 	}
 }
 
-// Play a single round and declare winner
-function playSingleGame(playerSelection: string, computerSelection: string): string {
-	const playerChoice = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
-	const computerChoice = computerSelection.slice(0,1).toUpperCase() + computerSelection.slice(1).toLowerCase();
+// Play a single round and declare winner of round
+function playSingleRound(playerSelection: string, computerSelection: string): string {
+	const playerChoice = playerSelection;	
+	const computerChoice = computerSelection; 
 	const messageDraw = `Its a draw! ${playerChoice} vs ${computerChoice}`;
 	const messagePlayerWin = `The player wins! ${playerChoice} vs ${computerChoice}`;
 	const messageComputerWin = `The computer wins! ${computerChoice} vs ${playerChoice}`;
 	if(playerChoice == computerChoice) {
 		return messageDraw;
-	} else if (playerChoice == "Rock" && computerChoice != "Paper") {
-		playerScore++
+	} else if (playerChoice == "rock" && computerChoice != "paper") {
+		playerScore++;
+		playerScoreDiv.innerText = playerScore.toString();
 		return messagePlayerWin;
-	} else if (playerChoice == "Scissor" && computerChoice != "Rock") {
-		playerScore++
+	} else if (playerChoice == "scissor" && computerChoice != "rock") {
+		playerScore++;
+		playerScoreDiv.innerText = playerScore.toString();
 		return messagePlayerWin;
-	} else if (playerChoice == "Paper" && computerChoice != "Scissor") {
-		playerScore++
+	} else if (playerChoice == "paper" && computerChoice != "scissor") {
+		playerScore++;
+		playerScoreDiv.innerText = playerScore.toString();
 		return messagePlayerWin;
 	} else {
-		computerScore++
+		computerScore++;
+		computerScoreDiv.innerText = computerScore.toString();
 		return messageComputerWin;
 	}
 }
 
-// Play a game
-function playGame() {
-	console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-	console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-	console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-	console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-	console.log(playSingleGame(prompt("Choose rock, paper or scissor"), getComputerChoice()));
-	if (playerScore > computerScore) {
-		console.log(`Congratulations player! You won with ${playerScore} points vs the computers ${computerScore} points`) 
-	} else if (playerScore < computerScore) {	
-		console.log(`I'm sorry player! You lost with ${playerScore} points vs the computers ${computerScore} points`)
-	} else {
-		console.log(`It's a tie! You both got ${playerScore} points! `)
+function endGame(result: number) {
+	
+	inputButtons.forEach((btn) => {
+		btn.disabled = true;
+	})
+	
+	if (result > 0) {
+		winnerPresentation.innerText = `Game over! Congratulations player! You won with ${playerScore} points vs the computers ${computerScore} points`; 
+	} else {	
+		winnerPresentation.innerText = `Game over! I'm sorry player! You lost with ${playerScore} points vs the computers ${computerScore} points`;
 	}
+	
+	let resetBtn = document.createElement("button");
+	resetBtn.value = "play-again";
+	resetBtn.textContent = "Play again";
+	resetBtn.classList.add("reset-button");
+	resetBtn.addEventListener("click", function(){
+		resetGame(resetBtn);
+	});
+	scoreBoard.appendChild(resetBtn);
 }
-playGame();
+
+function  resetGame(element: HTMLButtonElement): void {
+	playerScore = 0;
+	computerScore = 0;
+	computerScoreDiv.innerText = computerScore.toString();
+	playerScoreDiv.innerText = playerScore.toString();
+	inputButtons.forEach(e => e.disabled = false);
+	scoreBoard.removeChild(element);
+};
